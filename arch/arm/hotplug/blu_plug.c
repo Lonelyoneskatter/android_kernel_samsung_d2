39261e346803a9ee58f8700fea915eddea3a486f
Pranav Vashi <neobuddy89@gmail.com>
2016-09-13 07:29:25 -0700

    Squash: blu_plug
     * msm: blu_plug: Use state notifier API
     - More reliable.
    
     * blu_plug: Improve routines
     - Remove freq modifications. This is hotplug driver.
      So, delegate only hotplug work here.
     - Remove inlining every fcking thing and resolve section mismatches.
     - Simplify suspend/resume routine calls. Remove special
      workqueues for them as State Notifier API takes
      care of sequential calls.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
     * blu_plug: Convert direct references to HZ
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
     * blu_plug: Pull siblings online when exiting
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
     * blu_plug: Store parameters properly
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
     * blu_plug: Store parameters properly
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
     * Fix small compilation error on d2
    Signed-off-by: Kenny Escobar <q.escobar14@gail.com>

--- arch/arm/hotplug/blu_plug.c
+++ arch/arm/hotplug/blu_plug.c
@@ -21,27 +21,23 @@
 #include <linux/workqueue.h>
 #include <linux/sched.h>
 #include <linux/timer.h>
-#include <linux/lcd_notify.h>
 #include <linux/cpufreq.h>
-#include <mach/cpufreq.h>
 #include <linux/delay.h>
 #include <linux/slab.h>
-#include <linux/input.h>
-#include <linux/timer.h>
-#include <linux/fb.h>
+#ifdef CONFIG_STATE_NOTIFIER
+#include <linux/state_notifier.h>
+#endif
 
-#define INIT_DELAY		(60 * HZ) /* Initial delay to 60 sec, 4 cores while boot */
-#define DELAY			(HZ / 2)
-#define UP_THRESHOLD		(80)
-#define MIN_ONLINE		(1)
-#define MAX_ONLINE		(2)
-#define DEF_DOWN_TIMER_CNT	(6)	/* 3 secs */
-#define DEF_UP_TIMER_CNT	(2)	/* 1 sec */
-#define MAX_CORES_SCREENOFF     (1)
-#define MAX_FREQ_SCREENOFF      (0)
-#define MAX_FREQ_PLUG           (702000)
-#define DEF_PLUG_THRESHOLD      (70)
-#define BLU_PLUG_ENABLED	(0)
+#define INIT_DELAY		20000
+#define DELAY			100
+#define UP_THRESHOLD		80
+#define MIN_ONLINE		1
+#define MAX_ONLINE		2
+#define DEF_DOWN_TIMER_CNT	6
+#define DEF_UP_TIMER_CNT	2
+#define MAX_CORES_SCREENOFF     1
+#define DEF_PLUG_THRESHOLD      70
+#define BLU_PLUG_ENABLED	0
 
 static unsigned int blu_plug_enabled = BLU_PLUG_ENABLED;
 
@@ -54,18 +50,14 @@ static unsigned int up_timer;
 static unsigned int down_timer_cnt = DEF_DOWN_TIMER_CNT;
 static unsigned int up_timer_cnt = DEF_UP_TIMER_CNT;
 static unsigned int max_cores_screenoff = MAX_CORES_SCREENOFF;
-static unsigned int max_freq_screenoff = MAX_FREQ_SCREENOFF;
-static unsigned int max_freq_plug = MAX_FREQ_PLUG;
 static unsigned int plug_threshold[MAX_ONLINE] = {[0 ... MAX_ONLINE-1] = DEF_PLUG_THRESHOLD};
 
 static struct delayed_work dyn_work;
 static struct workqueue_struct *dyn_workq;
-static struct work_struct suspend, resume;
 static struct notifier_block notify;
 
-
 /* Bring online each possible CPU up to max_online cores */
-static inline void up_all(void)
+static void __ref up_all(void)
 {
 	unsigned int cpu;
 
@@ -76,23 +68,13 @@ static inline void up_all(void)
 	down_timer = 0;
 }
 
-/* Put offline each possible CPU down to min_online threshold */
-static inline void down_all(void)
-{
-	unsigned int cpu;
-
-	for_each_online_cpu(cpu)
-		if (cpu && num_online_cpus() > min_online)
-			cpu_down(cpu);
-}
-
 /* Iterate through possible CPUs and bring online the first offline found */
-static inline void up_one(void)
+static void __ref up_one(void)
 {
 	unsigned int cpu;
 
 	/* All CPUs are online, return */
-	if (num_online_cpus() == max_online)
+	if (num_online_cpus() >= max_online)
 		goto out;
 
 	cpu = cpumask_next_zero(0, cpu_online_mask);
@@ -114,10 +96,8 @@ static inline void down_one(void)
 	bool all_equal = false;
 
 	/* Min online CPUs, return */
-	if (num_online_cpus() == min_online)
+	if (num_online_cpus() <= min_online)
 		goto out;
-
-	get_online_cpus();
 
 	for_each_online_cpu(cpu) {
 		unsigned int thres = plug_threshold[cpu];
@@ -142,8 +122,6 @@ static inline void down_one(void)
 		}
 	}
 
-	put_online_cpus();
-
 	if (all_equal)
 		cpu_down(l_cpu);
 	else
@@ -159,113 +137,77 @@ out:
  * If the average load is below up_threshold offline one more CPU if the
  * down_timer has expired.
  */
-static __ref void load_timer(struct work_struct *work)
+static void load_timer(struct work_struct *work)
 {
 	unsigned int cpu;
 	unsigned int avg_load = 0;
-	
+	unsigned int online_cpus = num_online_cpus();
+
 	if (down_timer < down_timer_cnt)
 		down_timer++;
 
 	if (up_timer < up_timer_cnt)
 		up_timer++;
-	
+
 	for_each_online_cpu(cpu)
-		avg_load;
+		avg_load++;
 		
-	avg_load /= num_online_cpus();
-	
+	avg_load /= online_cpus;
+
 #if DEBUG
 	pr_debug("%s: avg_load: %u, num_online_cpus: %u\n", __func__, avg_load, num_online_cpus());
 	pr_debug("%s: up_timer: %u, down_timer: %u\n", __func__, up_timer, down_timer);
 #endif
 
-	if (avg_load >= up_threshold && up_timer >= up_timer_cnt)
+	if ((avg_load >= up_threshold && up_timer >= up_timer_cnt) ||
+		online_cpus < min_online)
 		up_one();
-	else if (down_timer >= down_timer_cnt)
+	else if (down_timer >= down_timer_cnt || online_cpus > max_online)
 		down_one();
 
-	queue_delayed_work_on(0, dyn_workq, &dyn_work, delay);
+	queue_delayed_work_on(0, dyn_workq, &dyn_work, msecs_to_jiffies(delay));
 }
 
-/* 
- * Manages driver behavior on screenoff mode
- * It sets max online CPUs to max_cores_screenoff and freq to max_freq_screenoff
- * Restores previous values on resume work
- *
- */
-static __ref void max_screenoff(bool screenoff)
+#ifdef CONFIG_STATE_NOTIFIER
+static void blu_plug_suspend(void)
 {
-	uint32_t cpu, freq;
-	
-	if (screenoff) {
-		max_freq_plug = cpufreq_quick_get_max(0);
-		freq = min(max_freq_screenoff, max_freq_plug);
+	int cpu;
 
-		cancel_delayed_work_sync(&dyn_work);
-		
-		for_each_possible_cpu(cpu) {
-			msm_cpufreq_set_freq_limits(cpu, MSM_CPUFREQ_NO_LIMIT, freq);
-			
-			if (cpu && num_online_cpus() > max_cores_screenoff)
-				cpu_down(cpu);
-		}
-		cpufreq_update_policy(cpu);
+	cancel_delayed_work_sync(&dyn_work);
+
+	for_each_possible_cpu(cpu) {
+		if (cpu != 0 && cpu_online(cpu)
+			&& num_online_cpus() > max_cores_screenoff)
+			cpu_down(cpu);
 	}
-	else {
-		freq = max_freq_plug;
-		
-		up_all();
-		
-		for_each_possible_cpu(cpu) {
-			msm_cpufreq_set_freq_limits(cpu, MSM_CPUFREQ_NO_LIMIT, freq);
-		}
-		cpufreq_update_policy(cpu);
-		
-		queue_delayed_work_on(0, dyn_workq, &dyn_work, delay);
+}
+
+static void blu_plug_resume(void)
+{
+	up_all();
+	queue_delayed_work_on(0, dyn_workq, &dyn_work, msecs_to_jiffies(delay));
+}
+
+static int state_notifier_callback(struct notifier_block *this,
+				unsigned long event, void *data)
+{
+	if (!blu_plug_enabled)
+		return NOTIFY_OK;
+
+	switch (event) {
+		case STATE_NOTIFIER_ACTIVE:
+			blu_plug_resume();
+			break;
+		case STATE_NOTIFIER_SUSPEND:
+			blu_plug_suspend();
+			break;
+		default:
+			break;
 	}
-	
-#if DEBUG
-	pr_debug("%s: num_online_cpus: %u, freq_online_cpus: %u\n", __func__, num_online_cpus(), freq);
+
+	return NOTIFY_OK;
+}
 #endif
-}
-
-/* On suspend put offline all cores except cpu0*/
-static void dyn_lcd_suspend(struct work_struct *work)
-{	
-	max_screenoff(true);
-}
-
-/* On resume bring online CPUs until max_online to prevent lags */
-static void dyn_lcd_resume(struct work_struct *work)
-{
-	max_screenoff(false);
-}
-
-static int fb_notifier_callback(struct notifier_block *self, unsigned long event, void *data)
-{
-	struct fb_event *evdata = data;
-	int *blank;
-
-	if (evdata && evdata->data && event == FB_EVENT_BLANK) {
-		blank = evdata->data;
-		switch (*blank) {
-			case FB_BLANK_UNBLANK:
-				//display on
-				queue_work_on(0, dyn_workq, &resume);
-				break;
-			case FB_BLANK_POWERDOWN:
-			case FB_BLANK_HSYNC_SUSPEND:
-			case FB_BLANK_VSYNC_SUSPEND:
-			case FB_BLANK_NORMAL:
-				//display off
-				queue_work_on(0, dyn_workq, &suspend);
-				break;
-		}
-	}
-
-	return 0;
-}
 
 /******************** Module parameters *********************/
 
@@ -278,10 +220,11 @@ static int set_up_threshold(const char *val, const struct kernel_param *kp)
 	ret = kstrtouint(val, 10, &i);
 	if (ret)
 		return -EINVAL;
+
 	if (i < 1 || i > 100)
 		return -EINVAL;
 
-	ret = param_set_uint(val, kp);
+	up_threshold = i;
 
 	return ret;
 }
@@ -294,7 +237,7 @@ static struct kernel_param_ops up_threshold_ops = {
 module_param_cb(up_threshold, &up_threshold_ops, &up_threshold, 0644);
 
 /* min_online */
-static __ref int set_min_online(const char *val, const struct kernel_param *kp)
+static int set_min_online(const char *val, const struct kernel_param *kp)
 {
 	int ret = 0;
 	unsigned int i;
@@ -302,15 +245,12 @@ static __ref int set_min_online(const char *val, const struct kernel_param *kp)
 	ret = kstrtouint(val, 10, &i);
 	if (ret)
 		return -EINVAL;
+
 	if (i < 1 || i > max_online || i > num_possible_cpus())
 		return -EINVAL;
 
-	ret = param_set_uint(val, kp);
-	
-	if (ret == 0) {
-			up_all();
-	}
-	
+	min_online = i;
+
 	return ret;
 }
 
@@ -322,7 +262,7 @@ static struct kernel_param_ops min_online_ops = {
 module_param_cb(min_online, &min_online_ops, &min_online, 0644);
 
 /* max_online */
-static __ref int set_max_online(const char *val, const struct kernel_param *kp)
+static int set_max_online(const char *val, const struct kernel_param *kp)
 {
 	int ret = 0;
 	unsigned int i;
@@ -330,16 +270,12 @@ static __ref int set_max_online(const char *val, const struct kernel_param *kp)
 	ret = kstrtouint(val, 10, &i);
 	if (ret)
 		return -EINVAL;
+
 	if (i < 1 || i < min_online || i > num_possible_cpus())
 		return -EINVAL;
 
-	ret = param_set_uint(val, kp);
-	
-	if (ret == 0) {
-		down_all();
-		up_all();
-	}
-	
+	max_online = i;
+
 	return ret;
 }
 
@@ -351,7 +287,7 @@ static struct kernel_param_ops max_online_ops = {
 module_param_cb(max_online, &max_online_ops, &max_online, 0644);
 
 /* max_cores_screenoff */
-static __ref int set_max_cores_screenoff(const char *val, const struct kernel_param *kp)
+static int set_max_cores_screenoff(const char *val, const struct kernel_param *kp)
 {
 	int ret = 0;
 	unsigned int i;
@@ -359,18 +295,15 @@ static __ref int set_max_cores_screenoff(const char *val, const struct kernel_pa
 	ret = kstrtouint(val, 10, &i);
 	if (ret)
 		return -EINVAL;
+
 	if (i < 1 || i > max_online || i > num_possible_cpus())
 		return -EINVAL;
-	if (i > max_online)
-		max_cores_screenoff = max_online;
 
-	ret = param_set_uint(val, kp);
-	
-	if (ret == 0) {
-		down_all();
-		up_all();
-	}
-	
+	if (i > max_online)
+		i = max_online;
+
+	max_cores_screenoff = i;
+
 	return ret;
 }
 
@@ -381,30 +314,6 @@ static struct kernel_param_ops max_cores_screenoff_ops = {
 
 module_param_cb(max_cores_screenoff, &max_cores_screenoff_ops, &max_cores_screenoff, 0644);
 
-/* max_freq_screenoff */
-static int set_max_freq_screenoff(const char *val, const struct kernel_param *kp)
-{
-	int ret = MAX_FREQ_SCREENOFF;
-	unsigned int i;
-
-	ret = kstrtouint(val, 10, &i);
-	if (ret)
-		return -EINVAL;
-	if (i < 35800 || i > 3091200)
-		return -EINVAL;
-
-	ret = param_set_uint(val, kp);
-	
-	return ret;
-}
-
-static struct kernel_param_ops max_freq_screenoff_ops = {
-	.set = set_max_freq_screenoff,
-	.get = param_get_uint,
-};
-
-module_param_cb(max_freq_screenoff, &max_freq_screenoff_ops, &max_freq_screenoff, 0644);
-
 /* down_timer_cnt */
 static int set_down_timer_cnt(const char *val, const struct kernel_param *kp)
 {
@@ -414,14 +323,15 @@ static int set_down_timer_cnt(const char *val, const struct kernel_param *kp)
 	ret = kstrtouint(val, 10, &i);
 	if (ret)
 		return -EINVAL;
+
 	if (i < 1 || i > 50)
 		return -EINVAL;
 		
 	if (i < up_timer_cnt)
-		down_timer_cnt = up_timer_cnt;
+		i = up_timer_cnt;
 
-	ret = param_set_uint(val, kp);
-	
+	down_timer_cnt = i;
+
 	return ret;
 }
 
@@ -441,10 +351,11 @@ static int set_up_timer_cnt(const char *val, const struct kernel_param *kp)
 	ret = kstrtouint(val, 10, &i);
 	if (ret)
 		return -EINVAL;
+
 	if (i < 1 || i > 50)
 		return -EINVAL;
 
-	ret = param_set_uint(val, kp);
+	up_timer_cnt = i;
 
 	return ret;
 }
@@ -463,60 +374,72 @@ module_param_array(plug_threshold, uint, NULL, 0644);
 
 static int dyn_hp_init(void)
 {
-	if (!blu_plug_enabled) {
+	if (!blu_plug_enabled)
 		return 0;
-	}
-	notify.notifier_call = fb_notifier_callback;
-	if (fb_register_client(&notify) != 0)
-		pr_info("%s: Failed to register FB notifier callback\n", __func__);
-	
+
+#ifdef CONFIG_STATE_NOTIFIER
+	notify.notifier_call = state_notifier_callback;
+	if (state_register_client(&notify))
+		pr_err("%s: Failed to register State notifier callback\n",
+			__func__);
+#endif
+
 	dyn_workq = alloc_workqueue("dyn_hotplug_workqueue", WQ_HIGHPRI | WQ_FREEZABLE, 0);
 	if (!dyn_workq)
 		return -ENOMEM;
 
-	INIT_WORK(&resume, dyn_lcd_resume);
-	INIT_WORK(&suspend, dyn_lcd_suspend);
 	INIT_DELAYED_WORK(&dyn_work, load_timer);
-	queue_delayed_work_on(0, dyn_workq, &dyn_work, INIT_DELAY);
+	queue_delayed_work_on(0, dyn_workq, &dyn_work, msecs_to_jiffies(INIT_DELAY));
 
 	pr_info("%s: activated\n", __func__);
 
 	return 0;
 }
 
-static void dyn_hp_exit(void)
+static void __ref dyn_hp_exit(void)
 {
+	int cpu;
+
 	cancel_delayed_work_sync(&dyn_work);
-	fb_unregister_client(&notify);
+
+#ifdef CONFIG_STATE_NOTIFIER
+	state_unregister_client(&notify);
+#endif
+
 	destroy_workqueue(dyn_workq);
+
+	/* Wake up all the sibling cores */
+	for_each_possible_cpu(cpu)
+		if (!cpu_online(cpu))
+			cpu_up(cpu);
 	
 	pr_info("%s: deactivated\n", __func__);
 }
-
 
 /* enabled */
 static int set_enabled(const char *val, const struct kernel_param *kp)
 {
 	int ret = 0;
 	unsigned int i;
-	int blu = 0;
 
 	ret = kstrtouint(val, 10, &i);
 	if (ret)
 		return -EINVAL;
+
 	if (i < 0 || i > 1)
-		return 0;
+		return -EINVAL;
 		
 	if (i == blu_plug_enabled)
-		return i;
+		return ret;
 
-	ret = param_set_uint(val, kp);
 	blu_plug_enabled = i;
-	if ((blu_plug_enabled == 1))
-		blu = dyn_hp_init();
-	if ((blu_plug_enabled == 0))
+
+	if (blu_plug_enabled)
+		ret = dyn_hp_init();
+	else
 		dyn_hp_exit();
-	return i;
+
+	return ret;
 }
 
 static struct kernel_param_ops enabled_ops = {
