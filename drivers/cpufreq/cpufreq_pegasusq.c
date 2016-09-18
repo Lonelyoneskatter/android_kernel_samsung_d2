3fd734067b85310b523b980143b492eec0375cf4
Lonelyoneskatter <threesixoh.skater@yahoo.com>
2016-09-18 08:32:47 -0700

    pegasusq: Set responsive frequency to 1188MHz

--- drivers/cpufreq/cpufreq_pegasusq.c
+++ drivers/cpufreq/cpufreq_pegasusq.c
@@ -57,7 +57,7 @@
 #define DEF_START_DELAY				(0)
 
 #define UP_THRESHOLD_AT_MIN_FREQ		(40)
-#define FREQ_FOR_RESPONSIVENESS			(2265600)
+#define FREQ_FOR_RESPONSIVENESS			(1188000)
 /* for fast decrease */
 #define FREQ_FOR_FAST_DOWN			(1574400)
 #define UP_THRESHOLD_AT_FAST_DOWN		(80)
