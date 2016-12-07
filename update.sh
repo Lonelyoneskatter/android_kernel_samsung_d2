dec4137764a3c5aed8c352060bf3a2ca846e068a
Lonelyoneskatter <threesixoh.skater@yahoo.com>
2016-12-07 01:53:49 -0800

    Update: Add update script for cm-14.1

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 # Small script to update kernel with cm
 git remote add upstream https://github.com/CyanogenMod/android_kernel_samsung_d2.git
 git fetch upstream
-git merge upstream/cm-13.0
+git merge upstream/cm-14.1
